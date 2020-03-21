cc6c70aca372f0baea069d60fb035b9cf260c09b
cxsong1 <csca12.cs@gmail.com>
2020-03-21 15:04:17 -0700

    sum and sumsq

--- stats.cpp
+++ stats.cpp
@@ -3,19 +3,60 @@
 
 stats::stats(PNG & im){
 
-// YOUR CODE HERE!!
-
+    this->im = im;
+    pair origin(0,0);
+    for(int x = 0; x < im.width(); x++){
+        for(int y = 0; y < im.height(); y++){
+            pair lr(x,y);
+            sumRed = getSum(r, origin, lr);
+            sumGreen = getSum(g, origin, lr);
+            sumBlue = getSum(b, origin, lr);
+            sumsqRed = getSumSq(r, origin, lr);
+            sumsqGreen = getSumSq(g, origin, lr);
+            sumsqBlue = getSumSq(b, origin, lr); 
+        }
+    }
 }
 
 long stats::getSum(char channel, pair<int,int> ul, pair<int,int> lr){
 
-// YOUR CODE HERE!!
+    int rollingSum = 0;
+    for(int x = ul.first; x < lr.first; x++){
+        for(int y = ul.second; y < lr.sencond; y++){
+            switch(channel){
+                case r:
+                    rollingSum += (int)im.getPixel(x,y)->r;
+                break;
+                case g:
+                    rollingSum += (int)im.getPixel(x,y)->g;
+                break;
+                case b:
+                    rollingSum += (int)im.getPixel(x,y)->b;
+                break;
+            }
+        }
+    }
 
 }
 
 long stats::getSumSq(char channel, pair<int,int> ul, pair<int,int> lr){
 
-// YOUR CODE HERE!!
+    int rollingSum = 0;
+        for(int x = ul.first; x < lr.first; x++){
+            for(int y = ul.second; y < lr.sencond; y++){
+                switch(channel){
+                    case r:
+                        rollingSum += ((int)im.getPixel(x,y)->r * (int)im.getPixel(x,y)->r);
+                    break;
+                    case g:
+                        rollingSum += ((int)im.getPixel(x,y)->g * (int)im.getPixel(x,y)->g);
+                    break;
+                    case b:
+                        rollingSum += ((int)im.getPixel(x,y)->b * (int)im.getPixel(x,y)->b);
+                    break;
+                }
+            }
+        }
 
 }
 
