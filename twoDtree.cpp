4f4881ec9b380fe5455910d0c6f485a396ed5aa3
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-04-05 12:15:26 -0700

    stuff

--- twoDtree.cpp
+++ twoDtree.cpp
@@ -133,7 +133,7 @@ int twoDtree::idealPrune(int leaves){
 	
 }
 
-int twoDtree::idealPruneRecursive(int leaves, int min, int max){
+int twoDtree::idealPruneRecursive(const int &leaves, const int &min, const int &max){
 	#if DEBUG
 	cout << "tolerance: " << ((min+max)/2) << endl;
 	#endif
@@ -151,7 +151,7 @@ int twoDtree::pruneSize(int tol){
 return pruneSizeRecursive(root, tol); 
 }
 
-int twoDtree::pruneSizeRecursive(Node* node, int tol){
+int twoDtree::pruneSizeRecursive(Node* node, int &tol){
 	if (node == NULL || (node->right == NULL && node->left == NULL) ) return 1; 
 	if (checkTol(node, node->avg, tol)){
 		return 1; 
@@ -165,7 +165,7 @@ void twoDtree::prune(int tol){
 }
 
 void twoDtree::pruneRecursive(Node* node, int tol){
-	//if (node == NULL || (node->right == NULL && node->left == NULL) ) return;
+	if (node == NULL || (node->right == NULL && node->left == NULL) ) return;
 	if (checkTol(node, node->avg, tol)){
 		clearRecursive(node->left);
 		clearRecursive(node->right);
@@ -173,13 +173,8 @@ void twoDtree::pruneRecursive(Node* node, int tol){
 		node->right = NULL;
 		return;
 	}
-	if(root->left != NULL){
-		pruneRecursive(root->left, tol);
-	}
-	if(root->right != NULL){
-		pruneRecursive(node->right, tol);
-	}
-
+	pruneRecursive(root->left, tol);
+	pruneRecursive(node->right, tol);
 }
 
 bool twoDtree::checkTol(Node* node, RGBAPixel avg, int tol){
